7d88fd9dcbb3d87c10b23f3b96456c038b983885
Cure-X <admin@cure-x.net>
2024-10-10 14:53:23 +0800

    split unterminated_comments.cpp

--- src/scanner/include/scanner/exceptions/lexical_errors.hpp
+++ src/scanner/include/scanner/exceptions/lexical_errors.hpp
@@ -5,25 +5,15 @@
 #include "lexical_error.hpp" //! (scanner)/exceptions/lexical_errors.hpp ?
 
 namespace lexical_errors {
-	class unterminated_comments : public lexical_point_error { //
+	class unterminated_comments : public lexical_point_error {
 	public:
-		unterminated_comments(int point) : lexical_point_error(point) {};
-
-		std::wstring info(language lang) {
-			switch (lang) {
-			case language::zh_cn:
-				return L"词法错误：未终结的注释";
-			default:
-				return L"lexical error: unterminated comments";
-			}
-		}
-
+		unterminated_comments(int pos);
+		std::wstring info(language lang);
 	};
 
 	class invalid_character : public lexical_point_error {
 	public:
 		invalid_character(int pos, wchar_t ch);
-
 		std::wstring info(language lang);
 	private:
 		wchar_t ch;
