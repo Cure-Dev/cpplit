3798c6c333be2c0e382bde0e5ad4019dfbeb4db4
Cure-X <admin@cure-x.net>
2024-10-10 14:49:02 +0800

    split lexical_error.hpp

--- src/scanner/include/scanner/exceptions/lexical_errors.hpp
+++ src/scanner/include/scanner/exceptions/lexical_errors.hpp
@@ -2,33 +2,7 @@
 
 #include "language.hpp"
 #include <string>
-
-class lexical_error {
-public:
-	virtual std::wstring info(language) = 0;
-};
-
-class lexical_point_error : public lexical_error {
-public:
-	lexical_point_error(int position) : position(position) {};
-	int get_pos() {
-		return position;
-	}
-
-private:
-	int position;
-};
-
-class lexical_segment_error : public lexical_error {
-public:
-	lexical_segment_error(std::pair<int, int> position) : position(position) {};
-	std::pair<int, int> get_pos() {
-		return position;
-	}
-
-private:
-	std::pair<int, int> position;
-};
+#include "lexical_error.hpp" //! (scanner)/exceptions/lexical_errors.hpp ?
 
 namespace lexical_errors {
 	class unterminated_comments : public lexical_point_error { //
